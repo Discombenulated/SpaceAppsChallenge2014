df0de433e324028478fc3fbaed1433ed1e78e043
Ben Noble <lonbn@Locals-MacBook-Pro.local>
2014-04-12 13:45:05 +0100

    Animating the iPhone background

--- iOSApp/Sky Snapper/Sky Snapper/HomeViewController.m
+++ iOSApp/Sky Snapper/Sky Snapper/HomeViewController.m
@@ -37,21 +37,63 @@
     //snapButton.layer.cornerRadius = 10;
 }
 
+NSTimer* backgroundAnimationTimer;
+
+-(void) viewWillAppear:(BOOL)animated {
+    [super viewWillAppear:animated];
+    [self.navigationController setNavigationBarHidden:YES animated:NO];
+    backgroundAnimationTimer = [NSTimer scheduledTimerWithTimeInterval:0.05f target:self selector:@selector(animateBackground) userInfo:nil repeats:true];
+}
+
+-(void) viewWillDisappear:(BOOL)animated {
+    [super viewWillDisappear:animated];
+    [self.navigationController setNavigationBarHidden:NO animated:NO];
+    [backgroundAnimationTimer invalidate];
+    backgroundAnimationTimer = nil;
+}
+
 - (void)didReceiveMemoryWarning
 {
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
 }
 
-/*
+- (UIStatusBarStyle)preferredStatusBarStyle
+{
+    return UIStatusBarStyleLightContent;
+}
+
 #pragma mark - Navigation
 
-// In a storyboard-based application, you will often want to do a little preparation before navigation
-- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender
-{
-    // Get the new view controller using [segue destinationViewController].
-    // Pass the selected object to the new view controller.
+ - (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+     if ([segue.identifier isEqualToString:@"SnapPictureSegue"]) {
+         UIImagePickerController *pickerController = segue.destinationViewController;
+         pickerController.delegate = self;
+         pickerController.sourceType = UIImagePickerControllerSourceTypeCamera;
+         pickerController.cameraCaptureMode = UIImagePickerControllerCameraCaptureModePhoto;
+         pickerController.cameraDevice = UIImagePickerControllerCameraDeviceRear;
+     }
+ }
+
+bool reverseAnimate = NO;
+
+-(void) animateBackground {
+    NSLog(@"Moving");
+    long step = 1.0f;
+    CGRect imageFrame = self.backgroundImage.frame;
+    
+    if (reverseAnimate){
+        imageFrame.origin = CGPointMake(imageFrame.origin.x + step, imageFrame.origin.y);
+        if (imageFrame.origin.x < 1.0) {
+            reverseAnimate = NO;
+        }
+    } else {
+        imageFrame.origin = CGPointMake(imageFrame.origin.x - step, imageFrame.origin.y);
+        if (imageFrame.origin.x - step > imageFrame.size.width - self.view.frame.size.width) {
+            reverseAnimate = YES;
+        }
+    }
+    self.backgroundImage.frame = imageFrame;
 }
-*/
 
 @end
