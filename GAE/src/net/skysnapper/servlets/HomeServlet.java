601c2be3b7055bab316297f7f16ee267bc9f8a38
James Shepherd <james.shepherd@paconsulting.com>
2014-04-12 21:02:41 +0100

    photodata servlet

--- GAE/src/net/skysnapper/servlets/HomeServlet.java
+++ GAE/src/net/skysnapper/servlets/HomeServlet.java
@@ -8,6 +8,11 @@ import net.skysnapper.util.JSPs;
 
 public class HomeServlet extends HttpServlet {
 	
+	/**
+	 * generated version.
+	 */
+	private static final long serialVersionUID = 5456686170284216609L;
+
 	@Override
 	public void doGet(HttpServletRequest req, HttpServletResponse resp) {
 		try {
