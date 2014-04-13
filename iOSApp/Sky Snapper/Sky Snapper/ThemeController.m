9a78475497296913e18dd1ce276c1217da458434
David Stanton <david.stanton@paconsulting.com>
2014-04-13 01:10:05 +0100

    navigation working and navigation bars hidden

--- iOSApp/Sky Snapper/Sky Snapper/ThemeController.m
+++ iOSApp/Sky Snapper/Sky Snapper/ThemeController.m
@@ -17,7 +17,19 @@
     
     initialHelpView.modalPresentationStyle = UIModalPresentationFormSheet;
     [self presentModalViewController:initialHelpView animated:YES];
+    
 }
+
+-(void) viewWillAppear:(BOOL)animated {
+    [super viewWillAppear:animated];
+    [self.navigationController setNavigationBarHidden:YES animated:NO];
+}
+
+-(void) viewWillDisappear:(BOOL)animated {
+    [super viewWillDisappear:animated];
+    [self.navigationController setNavigationBarHidden:NO animated:NO];
+}
+
 
 
 @end
