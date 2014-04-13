1fafab6fd6234df859db284e739bb82de678b8f0
George Buckingham <george.buckingham@paconsulting.com>
2014-04-13 07:56:11 +0100

    Filter bar changes and add beginning of filtering

--- GAE/war/javascript/snapper.js
+++ GAE/war/javascript/snapper.js
@@ -7,4 +7,51 @@ $(document).ready(function() {
 	if ($("#explore-maps-canvas").length > 0) {
 		$("#explore-maps-canvas").height($(document).height() - $("#explore-maps-canvas").offset().top - 5);
 	}
+	
+	window.markers = [];
+	
 });
+
+function zoom_changed(map) {
+	
+	console.log(map.zoomLevel());
+	
+}
+
+function Marker(lat, lng, icon, img) {
+	this.lat_ = lat;
+	this.lng_ = lng;
+	this.icon_ = icon;
+	this.img_ = img;
+	this.marker_ = null;
+}
+
+Marker.prototype.setMap = function(map) {
+	this.marker_ = new google.maps.Marker({
+		position: new google.maps.LatLng(this.lat_, this.lng_),
+		icon: this.icon_,
+		map: map,
+	});
+}
+
+Marker.prototype.remove = function() {
+	tihs.marker_.setMap(null);
+}
+var coloursbase = "/images/colours/";
+var orangeImage = {
+		  url: coloursbase + "orange.png",
+  		  size: new google.maps.Size(10, 10),
+  			scaledSize: new google.maps.Size(10, 10)
+  		};
+
+var darkBlueImage = {
+		  url: coloursbase + "dark-blue.png",
+		  size: new google.maps.Size(10, 10),
+			scaledSize: new google.maps.Size(10, 10)
+		};
+
+var paleBlueImage =  {
+		  url: coloursbase + "pale-blue.png",
+  		  size: new google.maps.Size(10, 10),
+  			scaledSize: new google.maps.Size(10, 10)
+  		};
